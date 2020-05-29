c28a8bd58d9985fc7a65e6cebc94680277a9f683
gomesdossantos <guilherme.santos165@fatec.sp.gov.br>
2020-05-28 21:04:39 -0300

    [#28] Importação de Classes Necessárias Para Realizar os Testes (JUnit 5)

--- src/test/java/api/restful/ApiRestfulApplicationTests.java
+++ src/test/java/api/restful/ApiRestfulApplicationTests.java
@@ -1,15 +1,41 @@
 package api.restful;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @Transactional
 @Rollback
 class ApiRestfulApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void insertInCatalog() {
+
+	}
+
+	@Test
+	public void insertInCoordinate() {
+
+	}
+
+	@Test
+	public void searchInCatalog() {
+
+	}
+
+	@Test
+	public void  deleteCoordinate() {
+
+	}
+
+	@Test
+	public void deleteCatalog() {
+
 	}
 
 }
