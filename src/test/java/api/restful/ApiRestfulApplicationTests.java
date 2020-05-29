59a9816033ad78e3bdfccda0c0e0b65477e39ce1
gomesdossantos <guilherme.santos165@fatec.sp.gov.br>
2020-05-28 21:34:20 -0300

    [#28] Inserção de Catálog no Banco como Teste

--- src/test/java/api/restful/ApiRestfulApplicationTests.java
+++ src/test/java/api/restful/ApiRestfulApplicationTests.java
@@ -1,5 +1,7 @@
 package api.restful;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.annotation.Rollback;
@@ -13,8 +15,34 @@ import org.springframework.boot.test.context.SpringBootTest;
 @Rollback
 class ApiRestfulApplicationTests {
 
+	@Autowired
+	private CatalogServiceImpl catalogService; 
+
 	@Test
 	public void insertInCatalog() {
+		Catalog catalog = new Catalog(
+			"clip_20170612T083546_Sigma0_VH_db",
+			"sentinel A image clip_Sigma0_VH_db.tif INPE",
+			"VH",
+			"2017-06-12 08:35:46",
+			new ArrayList<Coordinate>(),
+			"http://www.dpi.inpe.br/agricultural-database/lem/dados/cenas/Sentinel1/20170612_S1A/clip_20170612T083546_Sigma0_VH_db.tif"
+		);
+
+		catalogService.add(catalog);
+		// First Step
+		assertTrue(catalog.getId() != null);
+		
+		// SET CATALOG 
+		/*
+		List<Coordinate> coordinates = new ArrayList<Coordinate>();
+		coordinates.add(new Coordinate(
+			"projection",
+			1.11,
+			2.1512522,
+			null
+		));
+		*/
 
 	}
 
