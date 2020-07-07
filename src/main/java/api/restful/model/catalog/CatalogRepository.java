393023d3b21fdf9bbece5c12bf979d894266ad65
AbnerErnaniADSFatec <abiner.anjos@gmail.com>
2020-07-07 18:34:12 -0300

    [#39] Implementando buscas com join

--- src/main/java/api/restful/model/catalog/CatalogRepository.java
+++ src/main/java/api/restful/model/catalog/CatalogRepository.java
@@ -3,7 +3,5 @@ package api.restful.model.catalog;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.repository.CrudRepository;
 
-import api.restful.model.catalog.Catalog;
-
 @Repository
-public interface CatalogRepository extends CrudRepository<Catalog, Long> {}
+public interface CatalogRepository extends CrudRepository<Catalog, Long> { }
