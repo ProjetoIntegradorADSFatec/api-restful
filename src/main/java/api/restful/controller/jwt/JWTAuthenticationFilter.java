2e226973d9d9f3ecb9c30faca7c0d1c7e8018bfb
AbnerErnaniADSFatec <abiner.anjos@gmail.com>
2020-07-08 16:23:07 -0300

    update response security token

--- src/main/java/api/restful/controller/jwt/JWTAuthenticationFilter.java
+++ src/main/java/api/restful/controller/jwt/JWTAuthenticationFilter.java
@@ -2,6 +2,8 @@ package api.restful.controller.jwt;
 
 import com.auth0.jwt.JWT;
 import api.restful.model.user.AuthUser;
+import api.restful.model.views.ResponseToken;
+import com.google.gson.Gson;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -15,6 +17,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -52,6 +55,13 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
             .withSubject(((User) auth.getPrincipal()).getUsername())
                 .withExpiresAt(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
                     .sign(HMAC512(SECRET.getBytes()));
+        ResponseToken response = new ResponseToken(200, token,"Use this token to API CRUD options");
+        String jsonString = new Gson().toJson(response);
+        PrintWriter out = res.getWriter();
         res.addHeader(HEADER_STRING, token);
+        res.setContentType("application/json");
+        res.setCharacterEncoding("UTF-8");
+        out.print(jsonString);
+        out.flush();
     }
 }
